9acf26df7adad4781a5dbac23f657ab8e4650534
marsh <marshhu@qq.com>
2019-09-10 17:59:33 +0800

    first commit

---
+++ src/views/category/index.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="app-container">
+    <el-row>
+      <el-col :span="24">
+       <el-button type="primary">添加分类</el-button>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <div class="block">
+          <p>分类列表</p>
+          <el-tree
+            :data="data"
+            show-checkbox
+            node-key="id"
+            default-expand-all
+            :expand-on-click-node="false"
+            :render-content="renderContent"
+          >
+          </el-tree>
+        </div>
+      </el-col>
+    </el-row>
+
+  </div>
+</template>
+<script>
+let id = 1000;
+
+export default {
+  data() {
+    const data = [
+      {
+        id: 1,
+        label: "一级 1",
+        children: [
+          {
+            id: 4,
+            label: "二级 1-1",
+            children: [
+              {
+                id: 9,
+                label: "三级 1-1-1"
+              },
+              {
+                id: 10,
+                label: "三级 1-1-2"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        id: 2,
+        label: "一级 2",
+        children: [
+          {
+            id: 5,
+            label: "二级 2-1"
+          },
+          {
+            id: 6,
+            label: "二级 2-2"
+          }
+        ]
+      },
+      {
+        id: 3,
+        label: "一级 3",
+        children: [
+          {
+            id: 7,
+            label: "二级 3-1"
+          },
+          {
+            id: 8,
+            label: "二级 3-2"
+          }
+        ]
+      }
+    ];
+    return {
+      data: JSON.parse(JSON.stringify(data)),
+      data: JSON.parse(JSON.stringify(data))
+    };
+  },
+
+  methods: {
+    append(data) {
+      const newChild = { id: id++, label: "testtest", children: [] };
+      if (!data.children) {
+        this.$set(data, "children", []);
+      }
+      data.children.push(newChild);
+    },
+
+    remove(node, data) {
+      const parent = node.parent;
+      const children = parent.data.children || parent.data;
+      const index = children.findIndex(d => d.id === data.id);
+      children.splice(index, 1);
+    },
+
+    renderContent(h, { node, data, store }) {
+      return (
+        <span class="custom-tree-node">
+          <span>{node.label}</span>
+          <span>
+            <el-button
+              size="mini"
+              type="text"
+              on-click={() => this.append(data)}
+            >
+              Append
+            </el-button>
+            <el-button
+              size="mini"
+              type="text"
+              on-click={() => this.remove(node, data)}
+            >
+              Delete
+            </el-button>
+          </span>
+        </span>
+      );
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.block {
+  padding: 10px;
+}
+</style>
